d5efec7c360d330c00479619e5b4d05d40d0185e
Kim <72332310+kimnle@users.noreply.github.com>
2024-10-03 21:05:07 +1000

    feat: sign in with spotify works
    Still stuff to do with auth tokens!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
+import { useSpotifyAuthContext } from './contexts/SpotifyAuthProvider';
 import { useThemeContext } from './contexts/ThemeContextProvider'
 
 function App() {
 
   const [currentTheme, toggleTheme] = useThemeContext();
+  const [redirectToAuthCodeFlow] = useSpotifyAuthContext();
 
   return (
     <>
       <button onClick={toggleTheme}>
         Toggle theme
+      </button>
+      <button onClick={redirectToAuthCodeFlow}>
+        Sign in via Spotify
       </button>
     </>
   )
